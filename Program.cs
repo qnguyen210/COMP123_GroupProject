72be1062155548019ebab3547c5858f0eb3ad5ca
qnguyen210 <qnguy120@my.centennialcollege.ca>
2025-03-25 00:05:41 -0400

    Added Program.cs just to test.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace COMP123_GroupProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Main!");
+            // Instantiate/use your classes from AccountsException.cs or enums.cs here
+            Console.ReadLine();
+        }
+    }
+}
